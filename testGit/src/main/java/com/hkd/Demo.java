ccf0f92e6620237f305d71b4f3b9e2e9caa55858
zsg <1724473740@qq.com>
2019-02-28 19:11:05 +0800

    修复bug

--- testGit/src/main/java/com/hkd/Demo.java
+++ testGit/src/main/java/com/hkd/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("添加一行");
-		syso("修改一行代码");
+		System.out.println("修改一行代码");
 	}
 }
