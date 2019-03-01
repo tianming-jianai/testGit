40902133e9c3daf90f8f986540edceef6579b58d
zsg <1724473740@qq.com>
2019-03-01 11:49:54 +0800

    solve conflict

--- testGit/src/main/java/com/hkd/Demo.java
+++ testGit/src/main/java/com/hkd/Demo.java
@@ -5,11 +5,9 @@ public class Demo {
 		System.out.println("Hello world");
 		System.out.println("添加一行");
 		System.out.println("修改一行代码");
-<<<<<<< HEAD
+
 		//abc
-=======
 		//
->>>>>>> branch 'master' of https://github.com/tianming-jianai/testGit.git
 		//fetch
 	}
 }
