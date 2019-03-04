832ef9d8349636cf936d1a3393f96e0d80b304d6
zsg <1724473740@qq.com>
2019-03-04 14:36:36 +0800

    删除stash

--- testGit/src/main/java/com/hkd/Demo1.java
+++ testGit/src/main/java/com/hkd/Demo1.java
@@ -3,8 +3,7 @@ package com.hkd;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("DEMO1 test stash");
-		//"修改了demo1文件内容，但这时需要pull远程代码才能提交，所以我就要先把本地代码先暂存起来"
-		//暂存之后，修改的代码将被隐藏起来。需要恢复才能使用。
+		
 		
 		
 	}
