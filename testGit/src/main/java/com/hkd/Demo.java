cad235907e2a193e6faf7129d7c74588797b9d34
zsg <1724473740@qq.com>
2019-03-04 14:19:21 +0800

    删除文件

--- testGit/src/main/java/com/hkd/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.hkd;
-
-public class Demo {
-	public static void main(String[] args) {
-		//test stash 
-        //在develop分支上修改
-	}
-}
--- testGit/src/main/java/com/hkd/Demo2.java
+++
@@ -1,8 +0,0 @@
-package com.hkd;
-
-public class Demo2 {
-		public static void main(String[] args) {
-			System.out.println("Demo2");
-			//在devlop分支上修改代码
-		}
-}
